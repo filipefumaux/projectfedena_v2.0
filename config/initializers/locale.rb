a9a360736713f09cff5af1a94c7535f38a4c4b23
Filipe Fumaux <filipefumaux@gmail.com>
2011-04-30 13:02:32 -0300

    Almost installed

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,6 +2,6 @@
  
 # tell the I18n library where to find your translations
 #I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
- 
+I18n.load_path += Dir.glob("#{RAILS_ROOT}/app/config/locales/**/*.yml")
 # set default locale to something other than :en
 I18n.default_locale = :pt
